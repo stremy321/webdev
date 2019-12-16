45f3b56cdcf3cbc2a41cc87aea5dc7f1cc6d0eb6
stremy321 <jessica.stremy@bryanuniveristy.com>
2019-12-15 21:09:24 -0500

    todolist

---
+++ fsw120/week1/todolist/todolist/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <input type="checkbox" />
+            <p>Walk the Dog</p>
+            
+            <input type="checkbox" />
+            <p>Take the car to the mechanic</p>
+            
+            <input type="checkbox" />
+            <p>Go to the Mall</p>
+            
+            <input type="checkbox" />
+            <p>Pick up my little brother at @ 2pm</p>
+    </div>
+  );
+}
+
+export default App;
