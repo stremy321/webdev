7240b57ea39a241c834da7667b68e1a638e29902
stremy321 <jessica.stremy@bryanuniveristy.com>
2019-12-29 22:40:15 -0500

    boxes

--- fsw120/week2/my-app/src/App.js
+++ fsw120/week2/my-app/src/App.js
@@ -5,61 +5,61 @@ function App() {
   return (
     <div classname="fruit">
       <div className="fruitbox">
-                <img src="https://timesofindia.indiatimes.com/thumb/msid-68374658,width-800,height-600,resizemode-4/68374658.jpg"/>
+            
                 <h3>Watermelon</h3>
                 <p>West Africa</p>
                 <p>Helps Hydrates</p>
             </div>
             <div className="fruitbox">
-                <img src="https://5.imimg.com/data5/LN/DQ/MY-54721038/jack-fruit-500x500.jpg"/>
+                
                 <h3>Jackfruit</h3>
                 <p>Southern India</p>
                 <p>prevents anaemia</p>
             </div>
             <div className="fruitbox">
-                <img src="https://www.bbcgoodfood.com/sites/default/files/editor_files/2019/09/avocado.jpg"/>
+                
                 <h3>Avacado</h3>
                 <p>Central Mexico</p>
                 <p>antioxidants that can protect your eyes</p>
             </div>
             <div className="fruitbox">
-                <img src="https://image.shutterstock.com/image-photo/slice-fresh-orange-isolated-on-260nw-380910001.jpg"/>
+                
                 <h3>Orange</h3>
                 <p>China</p>
                 <p>prevents skin damage</p>
             </div>
             <div className="fruitbox">
-                <img src="https://image.shutterstock.com/image-photo/fresh-red-apple-isolated-on-260nw-397222102.jpg"/>
+                
                 <h3>Apple</h3>
                 <p>Central Asia</p>
                 <p>lower risk of diabetes</p>
             </div>
             <div className="fruitbox">
-                <img src="https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-320-80.jpg"/>
+                
                 <h3>Banana</h3>
                 <p>Southeast Asia</p>
                 <p>Good in Fiber</p>
             </div>
             <div className="fruitbox">
-                <img src="https://cpb-ap-se2.wpmucdn.com/blogs.unimelb.edu.au/dist/3/41/files/2012/10/pineapple.jpeg"/>
+                
                 <h3>Pineapple</h3>
                 <p>Basil/Paraguay</p>
                 <p>Boost immunity</p>
             </div>
             <div className="fruitbox">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/f/fb/Carabao_mangoes_%28Philippines%29.jpg"/>
+                
                 <h3>Mango</h3>
                 <p>Eastern India</p>
                 <p>Improve heart health</p>
             </div>
             <div className="fruitbox">
-                <img src="https://www.aces.edu/wp-content/uploads/2019/04/strawberry-.jpg"/>
+                
                 <h3>Strawberry</h3>
                 <p>North America</p>
                 <p>Vitamin C and potassium</p>
             </div>
             <div className="fruitbox">
-                <img src="https://www.cookforyourlife.org/wp-content/uploads/2018/08/shutterstock_297857891-min-696x464.jpg"/>
+                
                 <h3>Bluberries</h3>
                 <p>North America</p>
                 <p>Lower blood pressure</p>
--- fsw120/week2/my-app/src/box.js
+++ fsw120/week2/my-app/src/box.js
@@ -4,10 +4,10 @@ function box(props) {
     console.log(props)
     return (
         <div className="fruitbox">
-            <img src={props.contact.imgUrl}/>
-            <h3>{props.contact.name}</h3>
-            <p>Origin: {props.contact.origin}</p>
-            <p>Uses: {props.contact.uses}</p>
+           
+            <h3>Fruits: {props.contact.name}</h3>
+            <p>Origin: {props.box.origin}</p>
+            <p>Uses: {props.box.uses}</p>
         </div>
     )
 }
