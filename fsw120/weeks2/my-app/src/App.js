3b3c10599c2e9c40909720ba82bca260667f2b0b
stremy321 <jessica.stremy@bryanuniveristy.com>
2019-12-29 22:05:11 -0500

    vacation spots

---
+++ fsw120/weeks2/my-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import vacation from './vacation';
+import vacadata from "./vacadata";
+
+
+function App() {
+  const vacationComponents = vaca.map(vacation => <vaca key={vacation.id} place={vacation.place} price={vacation.price} time={vacation.timeToGo} />)
+ const vacationprice = vaca.price
+
+  return (
+    <div>
+        {vacationComponents}            
+    </div>
+)
+}
+
+
+ 
+    
+export default App;
