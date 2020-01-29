6d477b9e5ab455f98e606cf203b07d9292aca967
stremy321 <jessica.stremy@bryanuniveristy.com>
2020-01-28 23:22:52 -0500

    singlepageapp

---
+++ fsw115/week6/singlepageapp/luke.js
@@ -0,0 +1,39 @@
+async function getData(){
+    
+    try {
+        const response = await axios.get("https://swapi.co/api/people/25") 
+        const homeworld = await axios.get(response.data.homeworld)
+        const species = await axios.get(response.data.species)
+        listHomeWorldToDOM(homeworld, response, species)
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+    
+}
+
+
+getData()
+
+function listHomeWorldToDOM(homeworld, luke, species){
+    const nameh1 = document.createElement('h1')
+    nameh1.textContent = luke.data.name
+    document.body.appendChild(nameh1)
+
+    const h1 = document.createElement('h1')
+    h1.textContent = homeworld.data.name
+    document.body.appendChild(h1)
+
+    const speciesh1 = document.createElement('h1')
+    speciesh1.textContent = species.data.name
+    document.body.appendChild(speciesh1)
+    
+}
+
+
+
+
+
+
+
