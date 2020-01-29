e75c43c86ca6850776a8ba16dbfd8a15221ef2eb
stremy321 <jessica.stremy@bryanuniveristy.com>
2020-01-28 21:40:38 -0500

    calculator

---
+++ fsw120/week6/calculator/my-app/src/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Calculator</h1>
+            <h3>Solve a problem</h3>
+        </div>
+    )
+}
+
+export default Home
