c4f966d89489ccee9af8303d04c9660e6da39507
stremy321 <jessica.stremy@bryanuniveristy.com>
2020-02-16 18:42:29 -0500

    math

---
+++ fsw125/week1/math/app.js
@@ -0,0 +1,6 @@
+const {sum, multiply, divide, subtract} = require("./math")
+
+console.log(sum(90, 100))
+console.log(multiply(12, 12))
+console.log(divide(60, 20))
+console.log(subtract(120, 10))
---
+++ fsw125/week1/math/math.js
@@ -0,0 +1,15 @@
+module.exports.sum = function (a, b){
+    return a + b
+}
+
+module.exports.multiply = function (c, d){
+    return c * d
+}
+
+module.exports.divide = function (e, f){
+    return e / f
+}
+
+module.exports.subtract = function (g, h){
+    return g - f
+}
